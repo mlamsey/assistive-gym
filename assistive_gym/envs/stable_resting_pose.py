82e5c58480b33d55f1b3feaac7b2ae6747277e14
mlamsey <31256490+mlamsey@users.noreply.github.com>
2021-11-17 17:14:18 -0500

    basic sitting pose for human

--- assistive_gym/envs/stable_resting_pose.py
+++ assistive_gym/envs/stable_resting_pose.py
@@ -3,30 +3,37 @@ import pybullet as p
 from .env import AssistiveEnv
 
 import math
+import os
 
 from .agents.human import Human
-from .agents.human import head_joints
+from .agents.human import right_arm_joints, left_arm_joints, torso_joints, head_joints
+controllable_joints = right_arm_joints + left_arm_joints + torso_joints + head_joints
 
 
 def configure_human(human):
     human.impairment = None
     # human.set_all_joints_stiffness(0.02)
-    human.set_whole_body_frictions(lateral_friction=5., spinning_friction=1., rolling_friction=1.)
+    human.set_whole_body_frictions(lateral_friction=50., spinning_friction=10., rolling_friction=10.)
 
     joint_pos = default_sitting_pose(human)
-    # joint_pos = []
-    human.setup_joints(joint_pos, use_static_joints=True, reactive_force=None)
+    human.setup_joints(joint_pos, use_static_joints=False, reactive_force=None)
 
-    start_pos = [0, 0, 0.425]
-    # start_orient = [math.sqrt(2)/2., 0., 0., -math.sqrt(2)/2.]
-    start_orient = [0.5, 0, 0, -0.8660254]
+    start_pos = [0, 0, 0.75]
+    start_orient = [0, 0, 0, 1]
     human.set_base_pos_orient(start_pos, start_orient)
     human.set_on_ground()
 
     joint_i = [pose[0] for pose in joint_pos]
     joint_th = [pose[1] for pose in joint_pos]
-    joint_gains = [0.5] * len(joint_i)
-    forces = [10.] * len(joint_i)
+    joint_gains = [10.] * len(joint_i)
+    forces = [50.] * len(joint_i)
+
+    # tweak joint control
+    for i in range(len(joint_gains)):
+        if i not in controllable_joints:
+            joint_gains[i] = 0.
+            forces[i] = 0.
+
     human.control(joint_i, joint_th, joint_gains, forces)
 
 # def set_joint_stiffnesses(human):
@@ -37,16 +44,16 @@ def default_sitting_pose(human):
     # Arms
     joint_pos = [(human.j_right_shoulder_x, 30.),
                  (human.j_left_shoulder_x, -30.),
-                 (human.j_right_shoulder_y, 60.),
-                 (human.j_left_shoulder_y, 60.),
-                 (human.j_right_elbow, -110.),
-                 (human.j_left_elbow, -110.)]
+                 (human.j_right_shoulder_y, 0.),
+                 (human.j_left_shoulder_y, 0.),
+                 (human.j_right_elbow, -90.),
+                 (human.j_left_elbow, -90.)]
 
     # Legs
-    joint_pos += [(human.j_right_knee, 45.),
-                  (human.j_left_knee, 45.),
-                  (human.j_right_hip_x, -45.),
-                  (human.j_left_hip_x, -45.)]
+    joint_pos += [(human.j_right_knee, 90.),
+                  (human.j_left_knee, 90.),
+                  (human.j_right_hip_x, -90.),
+                  (human.j_left_hip_x, -90.)]
 
     # Torso
     joint_pos += [(human.j_waist_x, 0.)]
@@ -73,6 +80,8 @@ class BasePoseEnv(AssistiveEnv):
     def reset(self):
         super(BasePoseEnv, self).reset()
         self.build_assistive_env(fixed_human_base=False)
+        plane_path = os.path.join(self.directory, "primitives", "plane_chair.urdf")
+        plane_chair = p.loadURDF(plane_path, flags=p.URDF_USE_SELF_COLLISION, physicsClientId=self.id)
 
         # Human init
         configure_human(self.human)
@@ -83,6 +92,6 @@ class BasePoseEnv(AssistiveEnv):
 
 class StableRestingPoseEnv(BasePoseEnv):
     def __init__(self):
-        human = Human(controllable_joint_indices=head_joints, controllable=True)
+        human = Human(controllable_joint_indices=controllable_joints, controllable=True)
         super(BasePoseEnv, self).__init__(human=human)
 
